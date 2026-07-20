77ff3e35872d5b9a7a0b15156b2d4a93f25db8bc
danny <58836052+dannymendiola@users.noreply.github.com>
2026-07-19 19:17:35 -0700

    initial commit

---
+++ src/client/java/com/dannymendiola/copper_ledgers/client/CopperLedgersClient.java
@@ -0,0 +1,10 @@
+package com.dannymendiola.copper_ledgers.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperLedgersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/dannymendiola/copper_ledgers/client/CopperLedgersDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dannymendiola.copper_ledgers.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperLedgersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
